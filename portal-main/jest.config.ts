376ed5206fd7579da754adaea74a3bfd981d5ebd
upiksaleh <upxsal@gmail.com>
2023-05-20 15:51:40 +0800

    initial

---
+++ portal-main/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'bolsel-portal-main',
+  preset: '../../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/apps/bolsel/portal-main',
+};
