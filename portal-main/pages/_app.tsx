5bde92ad3e3671c838519c608b8b69253da9b39b
upiksaleh <upxsal@gmail.com>
2023-06-02 16:24:59 +0800

    GTM initialize

--- portal-main/pages/_app.tsx
+++ portal-main/pages/_app.tsx
@@ -6,10 +6,25 @@ import '../src/styles/main.scss';
 import {SWRConfig} from "swr";
 import LayoutDefaultMain from "../components/layout/main";
 import {UIConfigProvider} from "@portal-web/shared-ui";
+import { GTM_ID, pageview } from '../src/gtm';
+import Script from "next/script";
 
 function CustomApp({Component, pageProps}: AppProps) {
   return (
     <>
+      <Script
+        id="gtag-base"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${GTM_ID}');
+          `,
+        }}
+      />
       <Head>
         <title>
           Portal Resmi Pemerintah Kabupaten Bolaang Mongondow Selatan
--- portal-main/pages/_document.tsx
+++ portal-main/pages/_document.tsx
@@ -1,5 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-
+import { GTM_ID } from '../src/gtm';
 export default function Document() {
   return (
     <Html>
@@ -21,6 +21,14 @@ export default function Document() {
         />
       </Head>
       <body>
+      <noscript>
+        <iframe
+          src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        />
+      </noscript>
         <Main />
         <NextScript />
       </body>
