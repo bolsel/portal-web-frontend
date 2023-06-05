4f8879cd52766ab9f0cc021bb01ba0a1b4140b43
upiksaleh <upxsal@gmail.com>
2023-06-05 20:26:26 +0800

    update swr

--- websites/pages/_app.tsx
+++ websites/pages/_app.tsx
@@ -1,14 +1,15 @@
-import { AppProps } from 'next/app';
+import {AppProps} from 'next/app';
 import Head from 'next/head';
 // import './styles.css';
 import '@portal-web/shared-ui/presets/default/styles/global.css';
 import '../src/styles/main.scss';
 import BaseLayout from '../components/base/layout/base-layout';
-import { UIConfigProvider } from '@portal-web/shared-ui';
-import { BasePageProps } from '../src/types';
+import {UIConfigProvider} from '@portal-web/shared-ui';
+import {BasePageProps} from '../src/types';
+import {SWRConfig} from "swr";
 
-function CustomApp({ Component, pageProps, router }: AppProps<BasePageProps>) {
-  const { title, website, subTitle } = pageProps;
+function CustomApp({Component, pageProps, router}: AppProps<BasePageProps>) {
+  const {title, website, subTitle} = pageProps;
   const mainTitle = website
     ? router.asPath !== '/'
       ? `${title} | ${website.name}`
@@ -18,28 +19,33 @@ function CustomApp({ Component, pageProps, router }: AppProps<BasePageProps>) {
     <>
       <Head>
         <title>{mainTitle}</title>
-        {subTitle ? <meta name="description" content={subTitle} /> : ''}
+        {subTitle ? <meta name="description" content={subTitle}/> : ''}
       </Head>
-
-      <UIConfigProvider
-        config={{
-          icons: {
-            APIProviders: [
-              {
-                name: '',
-                resources: [
-                  'https://iconify.api.bolselkab.go.id',
-                  'https://api.iconify.design',
-                ],
-              },
-            ],
-          },
+      <SWRConfig
+        value={{
+          revalidateOnFocus: false,
         }}
       >
-        <BaseLayout {...pageProps} route={router.route}>
-          <Component {...pageProps} />
-        </BaseLayout>
-      </UIConfigProvider>
+        <UIConfigProvider
+          config={{
+            icons: {
+              APIProviders: [
+                {
+                  name: '',
+                  resources: [
+                    'https://iconify.api.bolselkab.go.id',
+                    'https://api.iconify.design',
+                  ],
+                },
+              ],
+            },
+          }}
+        >
+          <BaseLayout {...pageProps} route={router.route}>
+            <Component {...pageProps} />
+          </BaseLayout>
+        </UIConfigProvider>
+      </SWRConfig>
     </>
   );
 }
