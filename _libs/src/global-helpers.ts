57e8e0751b1f38f6f59626ab2f0ad4e97440594d
upiksaleh <upxsal@gmail.com>
2023-06-23 21:54:09 +0800

    format code

--- _libs/src/global-helpers.ts
+++ _libs/src/global-helpers.ts
@@ -3,7 +3,10 @@ import { trimSlashes } from '@portal-web/shared-base';
 import { useUIConfigContextValue } from '@portal-web/shared-ui';
 
 export const urlAssetCdn = (file: FileType | string) => {
-  const filename_disk = typeof file === 'string' ? file : (file.filename_disk ?? `${file.fileId}.${file.extension}`);
+  const filename_disk =
+    typeof file === 'string'
+      ? file
+      : file.filename_disk ?? `${file.fileId}.${file.extension}`;
   return `https://cdn.bolselkab.go.id/portal-assets/live/${filename_disk}`;
 };
 
